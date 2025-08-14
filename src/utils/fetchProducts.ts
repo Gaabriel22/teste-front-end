290ef2443f2239edf074067fd0226fd628e32dde
Gabriel Amaral <gabrielvieira2205@gmail.com>
2025-08-14 08:03:28 -0300

    feat(utils): adiciona função fetchProduct para buscar produtos

---
+++ src/utils/fetchProducts.ts
@@ -0,0 +1,10 @@
+import axios from "axios"
+import type { Product } from "../types/Product"
+import type { ApiResponse } from "../types/ApiResponse"
+
+export const fetchProduct = async (): Promise<Product[]> => {
+  const { data } = await axios.get<ApiResponse>(
+    "https://app.econverse.com.br/teste-front-end/junior/tecnologia/lista-produtos/produtos.json"
+  )
+  return data.products
+}
